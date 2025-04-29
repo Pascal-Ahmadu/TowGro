e298763b06042b5c8290cd2108fa74596f06521b
Pascal-Ahmadu <pascalally41@gmail.com>
2025-04-30 00:23:53 +0100

    chore: Add environment example file

--- test/jest-e2e.config.js
+++ test/jest-e2e.config.js
@@ -2,11 +2,17 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.(t|j)sx?$': 'babel-jest'
+    '^.+\\.(t|j)sx?$': ['babel-jest', {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript'
+      ]
+    }]
   },
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.jest.json',
+      isolatedModules: true
     },
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
@@ -14,6 +20,10 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
   moduleNameMapper: {
     '^@common/(.*)$': '<rootDir>/src/common/$1',
-    '^@test-utils/(.*)$': '<rootDir>/src/__tests__/utils/$1'
-  }
-};
+    '^@test-utils/(.*)$': '<rootDir>/src/__tests__/utils/$1',
+    '^src/(.*)$': '<rootDir>/src/$1'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(nest-winston|@nestjs|@socket.io)/)'
+  ]
+}
