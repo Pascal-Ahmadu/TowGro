07e66e1b6f2965385fdec505b598a6dbb7c02f75
Pascal-Ahmadu <pascalally41@gmail.com>
2025-04-29 23:57:47 +0100

    chore: Add environment example file

--- test/jest-e2e.config.js
+++ test/jest-e2e.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
   moduleNameMapper: {
     '^@common/(.*)$': '<rootDir>/src/common/$1',
